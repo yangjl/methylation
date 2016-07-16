65243658bbd8e5bb85d24887201eeb647dad37de
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-07-16 11:01:50 -0700

    re-commit some failed jobs due to poweroff.

--- profiling/0.SRA_align/0.E.3_align_test.R
+++ profiling/0.SRA_align/0.E.3_align_test.R
@@ -3,7 +3,6 @@
 
 library("farmeR")
 
-
 ########### alignment
 fq1 <- list.files(path="largedata/fastq", pattern="R1.fastq.gz$", full.names = TRUE)
 fq2 <- list.files(path="largedata/fastq", pattern="R2.fastq.gz$", full.names = TRUE)
@@ -11,15 +10,13 @@ fq2 <- list.files(path="largedata/fastq", pattern="R2.fastq.gz$", full.names = T
 #bamfiles <- list.files(path="/group/jrigrp4/BS_teo20/WGBS/BSM", pattern="bam$", full.names = TRUE)
 
 ### note: for alignment, "bam" col should not present.
-inputdf <- data.frame(fq1 = fq1[1], 
-                      fq2 = fq2[1],
-                      outbase = "JRA1")
+inputdf <- data.frame(fq1 = fq1[1],  fq2 = fq2[1], outbase = "JRA1")
 
 ### AGPv2
 run_bismark(inputdf, genome = "/home/jolyang/dbcenter/AGP/AGPv2",
             outdir = "/home/jolyang/Documents/Github/methylation/largedata/bismark", 
             N = 1, align = TRUE,
-            email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 8))
+            email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 16))
 
 ### pesudo-ref
 ### note: for alignment, "bam" col should not present.
@@ -27,4 +24,4 @@ inputdf <- data.frame(fq1 = fq1[1], fq2 = fq2[1], outbase = "pg_JRA1")
 run_bismark(inputdf, genome = "/home/jolyang/Documents/Github/methylation/largedata/pgenome",
             outdir = "/home/jolyang/Documents/Github/methylation/largedata/bismark", 
             N = 1, align = TRUE,
-            email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 8))
+            email = "yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 8))
