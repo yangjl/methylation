b787268af13d0de130fa18ac1ae64dcb6fd85f2d
Jinliang Yang <yangjl0930@gmail.com>
2016-07-18 14:04:21 -0700

    updates

--- profiling/0.SRA_align/0.E.2_fq_chk.R
+++ profiling/0.SRA_align/0.E.2_fq_chk.R
@@ -1,30 +1,40 @@
 ### Jinliang Yang
-### use Li Heng's De novo assembly based variant calling pipeline
+### check the quality of WGBS data
 ### 3/22/2016
 
+library("farmeR")
+
+### rename gzipped files
+files <- list.files(path="largedata/fastq/", pattern="fastq.lz4.fastq.gz", full.names=T)
+file.rename(from=files, to=gsub("fastq.lz4.", "", files))
+
+files <- list.files(path="largedata/fastq/", pattern="gz.qc", full.names=T)
+file.rename(from=files, to=gsub(".gz.qc", "fastq.gz.qc", files))
+
+### get information
 fq <- list.files(path="largedata/fastq", pattern="fastq.gz$", full.names = TRUE)
-df <- data.frame(fq=fq, out=paste0("largedata/fqchkm/", gsub(".*/", "", fq), ".qc"))
+df <- data.frame(fq=fq, out=paste0("largedata/fastq/", gsub(".*/", "", fq), ".qc"))
 
 ### run quality checking
-run_fastq_qc(df, q=20, email=NULL, runinfo = c(FALSE, "bigmemh", 1))
+run_fastq_qc(df, q=20, email="yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 2))
     
     
 ### get summary stat
-files <- list.files(path="largedata/fqchkm", pattern="qc", full.names=TRUE)
+files <- list.files(path="largedata/fastq", pattern="qc$", full.names=TRUE)
 res <- get_qc(files, genomesize=2500000000)
-write.table(res, "cache/teo20_fastq_qc.csv", sep=",", row.names=FALSE, quote=FALSE)
+write.table(res, "cache/wgbs_teo20_fastq_qc.csv", sep=",", row.names=FALSE, quote=FALSE)
 
 ### plot results
 
-res <- read.csv("cache/teo20_fastq_qc.csv")
+res <- read.csv("cache/wgbs_teo20_fastq_qc.csv")
 
 idx <- seq(from=1, to =40, by=2)
 dp <- res[idx,]
 dp$depth <- 2*dp$depth
-dp$file <- gsub(".*Sample_|_index.*", "", dp$file)
+dp$file <- gsub(".*fastq/|_.*", "", dp$file)
 
 par(mfrow=c(1,2))
-barplot(dp$avgQ, names.arg=dp$file, col="#8b2323", las=2, main="Avg Base Quality (N=20)")
-barplot(dp$depth, names.arg=dp$file, col="#458b74", las=2, main="Read Depth (N=20)")
+barplot(dp$avgQ, names.arg=dp$file, col="#bdb76b", las=2, main="Avg Base Quality (N=20)")
+barplot(dp$depth, names.arg=dp$file, col="#698b69", las=2, main="Read Depth (N=20)")
 
 
