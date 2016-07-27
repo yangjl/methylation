0f4a3aaa9219eb3430df3c50c9394493a8ae1e71
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-07-26 23:56:21 -0700

    updates

--- profiling/0.SRA_align/0.E.3_quality_trimming.R
+++ profiling/0.SRA_align/0.E.3_quality_trimming.R
@@ -11,6 +11,6 @@ fq2 <- list.files(path="largedata/wgbs_fq", pattern="R2.fastq.gz$", full.names=T
 
 inputdf <- data.frame(fq1=fq1, fq2=fq2, out1= gsub("_R1", "_R1_trimmed",  fq1), 
                       out2=gsub("_R2", "_R2_trimmed",  fq2))
-run_cutadapt(inputdf[1, ], ad1="AGATCGGAAGAGC", ad2="AGATCGGAAGAGC", q=20,
+run_cutadapt(inputdf[2:40, ], ad1="AGATCGGAAGAGC", ad2="AGATCGGAAGAGC", q=20,
              email="yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 1))
 
