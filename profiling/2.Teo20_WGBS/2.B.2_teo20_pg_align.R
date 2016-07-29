76e156c36d15d314f92e81eb017faf3b438a87fd
Jinliang Yang <yangjl0930@gmail.com>
2016-07-29 08:54:58 -0700

    updates

--- profiling/2.Teo20_WGBS/2.B.2_teo20_pg_align.R
+++ profiling/2.Teo20_WGBS/2.B.2_teo20_pg_align.R
@@ -8,7 +8,7 @@ idtab$idchar <- gsub("-", "", idtab$idchar)
 idtab$pid <- paste0("JR", idtab$plate)
 ########### alignment
 fq1 <- list.files(path="largedata/wgbs_fq", pattern="R1_trimmed.fastq.gz$", full.names = TRUE)
-fq2 <- list.files(path="largedata/wgbs_fq", pattern="R1_trimmed.fastq.gz$", full.names = TRUE)
+fq2 <- list.files(path="largedata/wgbs_fq", pattern="R2_trimmed.fastq.gz$", full.names = TRUE)
 
 #bamfiles <- list.files(path="/group/jrigrp4/BS_teo20/WGBS/BSM", pattern="bam$", full.names = TRUE)
 ### note: for alignment, "bam" col should not be present.
@@ -17,7 +17,7 @@ inputdf <- merge(inputdf, idtab[, c("idchar", "pid")], by.x="outbase", by.y="pid
 inputdf$genome <- paste0("$HOME/Documents/Github/methylation/largedata/wgbs_pgenome/", inputdf$idchar)
 
 ### AGPv2
-run_bismark(inputdf[1:19,], genome = NULL,
+run_bismark(inputdf, genome = NULL,
             outdir = "/home/jolyang/Documents/Github/methylation/largedata/wgbs_bismark", 
             N = 1, align = TRUE,
             email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 16))
