2e54670c66af979e358e45f0ed99875ecd70b814
Jinliang Yang <yangjl0930@gmail.com>
2016-08-18 16:44:53 -0700

    crap!

--- profiling/2.Teo20_WGBS/2.B.3_teo20_pg_align.R
+++ profiling/2.Teo20_WGBS/2.B.3_teo20_pg_align.R
@@ -9,8 +9,8 @@ idtab$pid <- paste0("JR", idtab$plate)
 
 ########### Note, move fq files to /group/jrigrp7
 ########### alignment
-fq1 <- list.files(path="/group/jrigrp7/jyang/methylation/largedata/wgbs_fq", pattern="R1_trimmed.fastq.gz$", full.names = TRUE)
-fq2 <- list.files(path="/group/jrigrp7/jyang/methylation/largedata/wgbs_fq", pattern="R2_trimmed.fastq.gz$", full.names = TRUE)
+fq1 <- list.files(path="/group/jrigrp3/jyang/methylation/largedata/wgbs_fq", pattern="R1_trimmed.fastq.gz$", full.names = TRUE)
+fq2 <- list.files(path="/group/jrigrp3/jyang/methylation/largedata/wgbs_fq", pattern="R2_trimmed.fastq.gz$", full.names = TRUE)
 
 #bamfiles <- list.files(path="/group/jrigrp4/BS_teo20/WGBS/BSM", pattern="bam$", full.names = TRUE)
 ### note: for alignment, "bam" col should not be present.
@@ -22,7 +22,7 @@ inputdf <- merge(inputdf, idtab[, c("idchar", "pid")], by.x="outbase", by.y="pid
 inputdf$genome <- paste0("$HOME/Documents/Github/methylation/largedata/wgbs_pgen/", inputdf$idchar)
 
 ### AGPv2
-run_bismark(inputdf, genome = NULL,
+run_bismark(inputdf[-1,], genome = NULL,
             outdir = "/home/jolyang/Documents/Github/methylation/largedata/wgbs_align", 
             N = 1, align = TRUE,
             email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", 16))
