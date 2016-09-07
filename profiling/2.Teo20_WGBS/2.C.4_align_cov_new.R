5e7c8411fcfe971edb22dc8b27f5560b96ba8d40
Jinliang Yang <yangjl0930@gmail.com>
2016-09-06 17:34:18 -0700

    get ratio and coverage

--- profiling/2.Teo20_WGBS/2.C.4_align_cov_new.R
+++ profiling/2.Teo20_WGBS/2.C.4_align_cov_new.R
@@ -2,100 +2,66 @@
 ### plot the stat of the teo20 methylation data
 
 
-library("data.table")
-
-### check the results
+### write a python script and run through all the samples
 library("farmeR")
-f1 <- list.files(path="largedata/wgbs_align/", pattern="PE_report.txt$", full.names=TRUE)
-res <- get_file2tab(files=f1, features="Mapping efficiency:\t", replace=F)
+
+files <- list.files(path="largedata/wgbs_align", pattern="pe.CX_report.txt", full.names=TRUE)
+
+df <- data.frame(input=files, output=files)
+df$output <- gsub("align", "smoothed", df$output)
+df$output <- gsub(".CX_report.txt", "", df$output)
+
+for(i in 1:19){
+    shid <- paste0("slurm-script/split_", i, ".sh")
+    command <- paste("python lib/splitcontext.py -i", df$input[i], "-o", df$output[i])
+    cat(command, file=shid, sep="\n", append=FALSE)
+}
+shcode <- paste("sh slurm-script/split_$SLURM_ARRAY_TASK_ID.sh", sep="\n")
+
+set_array_job(shid="slurm-script/run_split.sh", shcode=shcode,
+              arrayjobs="1-19", wd=NULL, jobid="split",
+              email= "yangjl0930@gmail.com",
+              run = c(TRUE, "bigmemm", 1))
 
 
-files <- list.files(path="largedata/wgbs_align/", pattern="pe.CX_report.txt", full.names=TRUE)
+### collect results and plot it
 
-
-cvg <- dtp <- data.frame()
-for(i in 1:length(files)){
-    res <- fread("largedata/wgbs_bismark/JRA2_pe.CX_report.txt")
+get_cov_ratio <- function(){
+    ### get coverage
+    files <- list.files(path="largedata/wgbs_smoothed", pattern="stat", full.names=TRUE)
+    out1 <- data.frame()
+    for(i in 1:20){
+        f <- read.table(files[i], header=TRUE)
+        f$file <- files[i]
+        out1 <- rbind(out1, f)
+    }
+    write.table(out1, "cache/wgbs_cov_09062016.csv", sep=",", row.names=FALSE, quote=FALSE)
     
-    cg <- res[V6 == "CG"]
-    chg <- res[V6 == "CHG"]
-    chh <- res[V6 == "CHH"]
     
-    rm(list="res")
+    ### get ratio of CG, CHG and CHH
+    fcg <- list.files(path="largedata/wgbs_smoothed", pattern="cg", full.names=TRUE)
+    fchg <- list.files(path="largedata/wgbs_smoothed", pattern="chg", full.names=TRUE)
+    fchh <- list.files(path="largedata/wgbs_smoothed", pattern="chh", full.names=TRUE)
     
-    cg$tot <- cg$V4 + cg$V5
-    chg$tot <- chg$V4 + chg$V5
-    chh$tot <- chh$V4 + chh$V5
-     
-    tem <- data.frame(cg=cg[,sum(tot != 0)]/nrow(cg), 
-                      chg=chg[,sum(tot != 0)]/nrow(chg), 
-                      chh=chh[,sum(tot != 0)]/nrow(chh))
+    out2 <- data.frame()
+    for(i in 1:20){
+        f1 <- fread(fcg[i])
+        r1 <- (f1[,sum(V4)] + f1[,sum(V5)])/nrow(f1)
+        
+        f2 <- fread(fchg[i])
+        r2 <- (f2[,sum(V4)] + f2[,sum(V5)])/nrow(f2)
+        
+        f3 <- fread(fchh[i])
+        r3 <- (f3[,sum(V4)] + f3[,sum(V5)])/nrow(f3)
+        tem <- data.frame(file=fcg[i], cg=r1, chg=r2, chh=r3)
+        out2 <- rbind(out2, tem)
+    }
+    write.table(out2, "cache/wgbs_ratio_09062016.csv", sep=",", row.names=FALSE, quote=FALSE)
 }
 
+#######
+get_cov_ratio()
 
 
 
-
-
-
-
-### CG, CHG and CHH
-###180125000, 158277169, 624401016
-res <- read.table("data/res.txt", header=FALSE)
-names(res) <- c("sample", "cov_CG", "cov_CHG", "cov_CHH", "ratio_CG", "ratio_CHG", "ratio_CHH",
-                "tot_CG", "tot_CHG", "tot_CHH")
-res$sample <- gsub(".*/|_methratio.*", "", res$sample)
-res$tot_CG <- res$tot_CG/180125000
-res$tot_CHG <- res$tot_CHG/158277169
-res$tot_CHH <- res$tot_CHH/624401016
-
-#library(tidyr)
-library(reshape2)
-resl <- melt(res, id.vars="sample")
-resl$variable <- as.character(resl$variable)
-resl$type <- gsub("_.*", "", resl$variable)
-resl$context <- gsub(".*_", "", resl$variable)
-
-
-
-p1 <- ggplot(subset(resl, type=="cov"), aes(x=context, y=value, fill=context)) +
-    geom_boxplot() +
-    theme_bw() +
-    theme(plot.title = element_text(color="red", size=20, face="bold.italic"),
-          axis.text.x = element_text(size=18),
-          axis.text.y = element_text(size=13),
-          axis.title = element_text(size=18, face="bold")) +
-    #scale_fill_manual(values=c("#008080", "#003366", "#40e0d0")) +
-    ggtitle("Sequencing Depth") + xlab("") + ylab("Depth per cytosine site") + 
-    guides(fill=FALSE)
-#guides(colour=FALSE, linetype=FALSE)
-
-p2 <- ggplot(subset(resl, type=="ratio"), aes(x=context, y=value, fill=context)) +
-    geom_boxplot() +
-    theme_bw() +
-    theme(plot.title = element_text(color="red", size=20, face="bold.italic"),
-          axis.text.x = element_text(size=18),
-          axis.text.y = element_text(size=13),
-          axis.title = element_text(size=18, face="bold")) +
-    #scale_fill_manual(values=c("#008080", "#003366", "#40e0d0")) +
-    ggtitle("Methylation Ratio") + xlab("") + ylab("Mean C/CT Ratio") + 
-    guides(fill=FALSE)
-
-p3 <- ggplot(subset(resl, type=="tot"), aes(x=context, y=value, fill=context)) +
-    geom_boxplot() +
-    theme_bw() +
-    theme(plot.title = element_text(color="red", size=20, face="bold.italic"),
-          axis.text.x = element_text(size=18),
-          axis.text.y = element_text(size=13),
-          axis.title = element_text(size=18, face="bold")) +
-    #scale_fill_manual(values=c("#008080", "#003366", "#40e0d0")) +
-    ggtitle("Coverage of cytosine sites") + xlab("") + ylab("covered / possible C sites") + 
-    guides(fill=FALSE)
-#guides(colour=FALSE, linetype=FALSE)
-
-source("~/Documents/Github/zmSNPtools/Rcodes/multiplot.R")
-#multiplot(p1, p4, p2, p5, p3, p6, cols=3)
-pdf("graphs/stat.pdf", width=16, height=5)
-multiplot(p3, p1, p2, cols=3)
-dev.off()
 
