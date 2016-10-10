1d4410fb28e95da0f4cf64b632011c81d1a74679
Jinliang Yang <yangjl0930@gmail.com>
2016-10-10 10:57:57 -0700

    move some DMR codes to old/

--- profiling/2.Teo20_WGBS/2.D.1_re_cal_C_sites.R
+++ profiling/2.Teo20_WGBS/2.D.1_re_cal_C_sites.R
@@ -14,14 +14,14 @@ library(data.table)
 source("lib/re_calc.R")
 
 ### CG
-file1 <- list.files(path="largedata/wgbs_smoothed", pattern="cg$", full.names = TRUE)
-out1 <- re_calc(file1[JOBID])
-write.table(out1, paste0(file1[JOBID], ".out"), sep=",", row.names=FALSE, quote=FALSE)
+#file1 <- list.files(path="largedata/wgbs_smoothed", pattern="cg$", full.names = TRUE)
+#out1 <- re_calc(file1[JOBID])
+#write.table(out1, paste0(file1[JOBID], ".out"), sep=",", row.names=FALSE, quote=FALSE)
 
 ## CHG
-file2 <- list.files(path="largedata/wgbs_smoothed", pattern="chg$", full.names = TRUE)
-out2 <- re_calc(file2[JOBID])
-write.table(out2, paste0(file2[JOBID], ".out"), sep=",", row.names=FALSE, quote=FALSE)
+#file2 <- list.files(path="largedata/wgbs_smoothed", pattern="chg$", full.names = TRUE)
+#out2 <- re_calc(file2[JOBID])
+#write.table(out2, paste0(file2[JOBID], ".out"), sep=",", row.names=FALSE, quote=FALSE)
 
 ## CHH
 file3 <- list.files(path="largedata/wgbs_smoothed", pattern="chh$", full.names = TRUE)
