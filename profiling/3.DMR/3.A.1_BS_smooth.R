083752a8ac6b4bb54c7866455974e48a31ce0abe
Jinliang Yang <yangjl0930@gmail.com>
2016-10-11 17:45:08 -0700

    updates

--- profiling/3.DMR/3.A.1_BS_smooth.R
+++ profiling/3.DMR/3.A.1_BS_smooth.R
@@ -14,49 +14,66 @@ library(data.table)
 library(bsseq)
 
 runbsmooth <- function(infile="largedata/wgbs_smoothed/JRA1_pe.cg.rc", outdir="largedata/COMET",
-                       cores=8){
+                       cores=8, chrs=1:10, BIN){
     
     meth <- fread(infile, data.table=FALSE)
     myid <- gsub(".*/|_.*", "", infile)
     myp <- paste(outdir, myid, sep="/")
-    dir.create(path=myp, showWarnings = TRUE)
-    for(i in 1:10){
+    dir.create(path=myp, showWarnings = FALSE)
+    
+    for(i in chrs){
         #tmp <- dat[dat$strand == "+",]
-        meth0 <- subset(meth, V2 == i)
+        message(sprintf("###>>> Working on [%s] at [chr%s] ...", myid, i))
+        
+        meth0 <- subset(meth, V2 %in% i)
         meth0$V2 <- paste0("chr", meth0$V2)
+       
         #meth0 <- subset(meth0, V7 > 0)
         meth0 <- meth0[order(meth0$V3), ]
         
-        BS <- BSseq(chr = meth0$V2, pos = meth0$V3,
-                    M = as.matrix(meth0$V5, ncol = 1),
-                    Cov = as.matrix(meth0$V7, ncol = 1), 
-                    sampleNames = myid)
-        
-        tmp <- meth0[meth0$V4 == "+",]
-        BS.forward <- BSseq(pos = tmp$V3, chr = tmp$V2, M = as.matrix(tmp$V5, ncol = 1),
-                            Cov = as.matrix(tmp$V7, ncol = 1), sampleNames = "forward")
-        tmp <- meth0[meth0$V4 == "-",]
-        BS.reverse <- BSseq(pos = tmp$V3, chr = tmp$V2, M = as.matrix(tmp$V5, ncol = 1),
-                            Cov = as.matrix(tmp$V7, ncol = 1), sampleNames = "reverse")
+        tmp1 <- meth0[meth0$V4 == "+",]
+        #tmp1 <- tmp1[1:10000,]
+        BS.forward <- BSseq(pos = tmp1$V3, chr = tmp1$V2, M = as.matrix(tmp1$V5, ncol = 1),
+                            Cov = as.matrix(tmp1$V7, ncol = 1), sampleNames = "forward")
+        tmp2 <- meth0[meth0$V4 == "-",]
+        #tmp2 <- tmp2[1:12000,]
+        BS.reverse <- BSseq(pos = tmp2$V3 - 1L, chr = tmp2$V2, M = as.matrix(tmp2$V5, ncol = 1),
+                            Cov = as.matrix(tmp2$V7, ncol = 1), sampleNames = "reverse")
         BS <- combine(BS.forward, BS.reverse)
         BS <- collapseBSseq(BS, columns = c("a", "a"))
-
         
-        res <- BSmooth(BS, ns = 70, h = 1000, maxGap = 10^8,
-                       parallelBy = c("sample", "chromosome"), mc.preschedule = FALSE,
-                       mc.cores = cores, keep.se = FALSE, verbose = TRUE)
-        
-        message(sprintf("###>>> output [chr%s] in [%s]", i, myp))
-        meth0$sm <- getMeth(res, type="smooth")
-        write.table( meth0[, c(2,3,3,8)], paste0(myp, "/", "chr", i, ".txt"), 
-                     sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+        tot <- ceiling(length(BS)/BIN)
+        for(b in 1:tot){
+            sline <- BIN*(b-1) + 1
+            eline <- BIN*b
+            myBS <- BS[sline:eline, ]
+            
+            message(sprintf("###>>> Smoothing [chr%s], bin [%s: %s - %s] ...", i, b, sline, eline))
+            res <- BSmooth(myBS, ns = 70, h = 1000, maxGap = 10^8,
+                           parallelBy = c("sample", "chromosome"), mc.preschedule = FALSE,
+                           mc.cores = 1, keep.se = FALSE, verbose = TRUE)
+            
+            df <- data.frame(seqnames=seqnames(res),
+                             starts=start(res),
+                             ends=end(res),
+                             scores=getMeth(res, type="smooth"))
+            
+            if(b == 1){
+                write.table(df, paste0(myp, "/", "chr", i, ".txt"), 
+                            sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+            }else{
+                write.table(df, paste0(myp, "/", "chr", i, ".txt"), 
+                            sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE, append = TRUE)
+            }
+        }
     }
 }
 
 ### test
-#runbsmooth(infile="largedata/wgbs_smoothed/JRA1_pe.cg.rc", outdir="largedata/COMET", cores=8)
+runbsmooth(infile="largedata/wgbs_smoothed/JRA1_pe.cg.rc", outdir="largedata/COMET", 
+           cores=1, chrs=10, BIN=1000000)
 
 ## CG
 file1 <- list.files(path="largedata/wgbs_smoothed", pattern="cg.rc$", full.names = TRUE)
-runbsmooth(infile=file1[JOBID], outdir="largedata/COMET", cores=8)
+runbsmooth(infile=file1[JOBID], outdir="largedata/COMET", cores=8, chrs=1:10)
 
