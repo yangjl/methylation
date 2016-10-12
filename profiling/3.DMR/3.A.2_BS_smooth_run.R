27f3fa49898110e83d64647921aa93426ef8f669
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-10-11 22:42:24 -0700

    up

--- profiling/3.DMR/3.A.2_BS_smooth_run.R
+++ profiling/3.DMR/3.A.2_BS_smooth_run.R
@@ -8,7 +8,7 @@ library("farmeR")
 run_Rcodes(inputdf=data.frame(file=1:20, out=1), outdir="slurm-script", cmdno=1,
            rcodes = "profiling/3.DMR/3.A.1_BS_smooth.R",
            arrayshid = "slurm-script/run_rcode_array.sh",
-           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 12, "96G"))
+           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 8, "60G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
 ###>>> RUN: sbatch -p bigmemm --mem 60G --ntasks=8 --time 24:00:00 slurm-script/run_rcode_array.sh
