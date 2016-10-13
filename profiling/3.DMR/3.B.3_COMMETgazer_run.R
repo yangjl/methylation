f66993f023e66c286cc98d2009f5a332db199bd2
Jinliang Yang <yangjl0930@gmail.com>
2016-10-12 17:08:43 -0700

    updates

--- profiling/3.DMR/3.B.3_COMMETgazer_run.R
+++ profiling/3.DMR/3.B.3_COMMETgazer_run.R
@@ -12,3 +12,4 @@ run_Rcodes(inputdf=data.frame(file=1:20, out=1), outdir="slurm-script", cmdno=1,
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
 ###>>> RUN: sbatch -p bigmemm --mem 60G --ntasks=8 --time 24:00:00 slurm-script/run_rcode_array.sh
+####>>> sbatch -p bigmemm --mem 16G --ntasks=2 --time 12:00:00 slurm-script/run_rcode_array.sh
