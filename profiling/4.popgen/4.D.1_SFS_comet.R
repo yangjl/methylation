ffc966aef83331b8e2ad02624c4a8dab1da23ae8
Jinliang Yang <yangjl0930@gmail.com>
2016-10-14 14:41:13 -0700

    run mcmc

--- profiling/4.popgen/4.D.1_SFS_comet.R
+++ profiling/4.popgen/4.D.1_SFS_comet.R
@@ -2,35 +2,37 @@
 ### 10-16-2016
 ### purpose: get SFS for features
 
-library(GenomicFeatures)
+
 library("data.table")
-
-res <- read.csv("cache/stat_exon_mean_var.csv")
-
-
-comet <- fread("largedata/COMET/CG_COMET/comet_blocks.csv", data.table=FALSE)
-
-
-get_comet_sfs <- function(comet, context="CG", cols=3:22){
+get_comet_sfs <- function(cometfile="largedata/COMET/CG_COMET/comet_blocks.csv", 
+                          outfile="cache/sfs_cg_comet.csv",
+                          cols=3:22){
+    comet <- fread(cometfile, data.table=FALSE)
     
     comet$miss <- apply(comet[, cols], 1, function(x){
         return(sum(x < 0))
     })
     comet <- subset(comet, miss == 0)
     ###########
-    
     f <- apply(comet[, cols], 1, function(x){
         n2 <- sum(x == 2)
         n1 <- sum(x == 1)
         return(2*n2 + n1)
     })
     sfs <- table(f)
+    write.table(sfs, outfile, sep=",", row.names=FALSE, quote=FALSE)
     return(sfs)
 }
 
-
-sfs <- getsfs(context="CHG", cols=3:22, BINSIZE=100)
-write.table(sfs, "cache/sfs_cg_comet.csv", sep=",", row.names=FALSE, quote=FALSE)
+###############
+files <- list.files(path="largedata/COMET/CG_COMET", pattern="chr10_comet", full.names = TRUE)
+for(i in 1:length(files)){
+    outf <- gsub(".*/", "cache/sfs_", files[i])
+    sfs <- get_comet_sfs(cometfile=files[i], 
+                         outfile=outf,
+                         cols=3:22)
+}
+    
 
 
 
--- profiling/4.popgen/4.D.4_comet_MCMC_setup.R
+++ profiling/4.popgen/4.D.4_comet_MCMC_setup.R
@@ -6,9 +6,9 @@
 
 library("farmeR")
 run_Rcodes(inputdf=data.frame(file=1:9, out=1), outdir="slurm-script", cmdno=1,
-           rcodes = "profiling/3.DMR/3.C.2_comet2blocks_sensitivity.R",
+           rcodes = "profiling/4.popgen/4.D.3_comet_MCMC.R",
            arrayshid = "slurm-script/run_rcode_array.sh",
-           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 4, "30G"))
+           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 2, "16G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
 ###>>> RUN: sbatch -p bigmemm --mem 30G --ntasks=4 --time 24:00:00 slurm-script/run_rcode_array.sh
