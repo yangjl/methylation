a33acd42a43a3e4b650a087f9563dbcd8821d7e4
Jinliang Yang <yangjl0930@gmail.com>
2016-10-18 17:30:49 -0700

    mark

--- profiling/4.popgen/4.E.5_feature_MCMC_plot.R
+++ profiling/4.popgen/4.E.5_feature_MCMC_plot.R
@@ -21,5 +21,43 @@ ob <- load("largedata/sfs_cg_comet_0.33_intron.RData")
 mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
 sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E8), sfsplot="plotmean", Ne=150000, k=0:40)
 
+###########
+ob <- load("largedata/ngbm_sfs_cg_comet_0.33_exon.RData")
+mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmean", Ne=150000, k=0:40)
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmode", Ne=150000, k=0:40)
+#posterior mu [ 5.19604993078061e-07 ], nu [ 1.23984784487354e-07 ] and s [ 4.84685320372646e-06 ]
+
+ob <- load("largedata/ngbm_sfs_cg_comet_0.33_gene.RData")
+mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmean", Ne=150000, k=0:40)
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmode", Ne=150000, k=0:40)
+#posterior mu [ 7.65364773455305e-07 ], nu [ 1.15642243453368e-07 ] and s [ 7.40731878049114e-06 ]
+
+ob <- load("largedata/ngbm_sfs_cg_comet_0.33_intron.RData")
+mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmean", Ne=150000, k=0:40)
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmode", Ne=150000, k=0:40)
+#posterior mu [ 7.70753203030945e-07 ], nu [ 1.10005583819213e-07 ] and s [ 8.22688696216015e-06 ]
+
+################ gbM
+ob <- load("largedata/gbm_sfs_cg_comet_0.33_exon.RData")
+mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmean", Ne=150000, k=0:40)
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmode", Ne=150000, k=0:40)
+#posterior mu [ 7.77170188577186e-07 ], nu [ 5.42423118338264e-08 ] and s [ 1.41743515403035e-05 ]
+
+ob <- load("largedata/gbm_sfs_cg_comet_0.33_gene.RData")
+mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmean", Ne=150000, k=0:40)
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmode", Ne=150000, k=0:40)
+#posterior mu [ 9.03339445303956e-07 ], nu [ 5.72949539919047e-08 ] and s [ 1.52062951256493e-05 ]
+
+ob <- load("largedata/gbm_sfs_cg_comet_0.33_intron.RData")
+mplot(res, burnin=0.2, rates=c(1E8,1E8, 1E5))
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmean", Ne=150000, k=0:40)
+sfsplot(res,burnin=0.2,rates=c(1E8,1E8,1E5), sfsplot="plotmode", Ne=150000, k=0:40)
+#posterior mu [ 7.53712629946411e-07 ], nu [ 4.37777207495372e-08 ] and s [ 1.45222758073796e-05 ]
+
 
 
