120dfb0b5a9cb18dbb2421b563f8f774a85c8eab
Jinliang Yang <yangjl0930@gmail.com>
2016-11-21 18:03:08 -0800

    running ibd

--- profiling/4.popgen/4.F.1_genomic_SFS.R
+++ profiling/4.popgen/4.F.1_genomic_SFS.R
@@ -48,15 +48,21 @@ write.table(ngbM[, c("seqname", "start", "end")], "largedata/gatk_vcf/non-gbm_ge
 ### convert to PLINK
 cmd1 <- "cd largedata/gatk_vcf"
 cmd2 <- "bcftools index -f JRI20_bi_snps_annot.vcf.gz"
+### extract regions of interest
 cmd3 <- paste("bcftools filter --regions-file gbm_gene_pos.txt JRI20_filtered_snps_annot.bcf.gz",
               "-Ou -o JRI20_gbm.bcf")
 cmd4 <- paste("bcftools filter --regions-file non-gbm_gene_pos.txt JRI20_filtered_snps_annot.bcf.gz",
               "-Ou -o JRI20_non-gbm.bcf")
 
+### compute frq
 cmd5 <- paste("plink -bcf JRI20_gbm.bcf --keep-allele-order --make-bed --out gbm_JRI20", 
               "--allow-extra-chr --freq")
+cmd6 <- paste("plink -bcf JRI20_non-gbm.bcf --keep-allele-order --make-bed --out non-gbm_JRI20", 
+              "--allow-extra-chr --freq")
+
+
 set_farm_job(slurmsh = "slurm-script/bcf2plink.sh",
-             shcode = c(cmd1, cmd2), wd = NULL, jobid = "bcf",
+             shcode = c(cmd1, cmd2, cmd3, cmd4, cmd5, cmd6), wd = NULL, jobid = "bcf",
              email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", "8"))
 
 
@@ -69,6 +75,13 @@ out1 <- table(frq1$count)
 
 write.table(as.data.frame(out1), "cache/gbm_sfs_genomic.csv", sep=",", row.names=FALSE, quote=FALSE)
 
+
+frq2 <- fread("largedata/gatk_vcf/non-gbm_JRI20.frq", header=TRUE)
+frq2 <- subset(frq2, NCHROBS == 40)
+frq2$count <- frq2$NCHROBS*frq2$MAF
+out2 <- table(frq2$count)
+
+write.table(as.data.frame(out2), "cache/non-gbm_sfs_genomic.csv", sep=",", row.names=FALSE, quote=FALSE)
 
 
 
