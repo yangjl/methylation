cd2d1a82b7078bfa3a010c67bf35af3bfe76f3cd
Jinliang Yang <yangjl0930@gmail.com>
2016-12-07 13:51:09 -0800

    updated plot

---
+++ profiling/3.DMR/3.D.2.1_COMET_feature_plot.R
@@ -0,0 +1,97 @@
+### Jinliang Yang
+### 12-07-2016
+### plot sfs with multiple features
+
+#####>>> read from cache/
+library("data.table")
+library("tidyr")
+library("ggplot2")
+library(cowplot)
+
+cbPalette=c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+
+###################################################################################################
+### SFS and bp
+df <- read.csv("cache/SFS_comet_blocks_CG.csv")
+dt <- as.data.table(df)
+tab1 <- dt[, .(bp = sum(length)), by= sfs] 
+tab2 <- data.frame(table(df$sfs))
+
+tab1 <- as.data.frame(tab1)
+plot(tab1$sfs, tab1$bp, type="h")
+tab <- merge(tab1, tab2, by.x="sfs", by.y="Var1")
+
+tab$bp <- tab$bp/sum(tab$bp)
+tab$Freq <- tab$Freq/sum(tab$Freq)
+
+out <- gather(tab, "type", "value", 2:3)
+
+s1 <- ggplot(out, aes(sfs, value, fill=type)) + geom_bar(stat="identity", position = "dodge") +
+    #theme_bw() +
+    xlab("SFS") +
+    ylab("Frequency") +
+    #scale_fill_manual(values=c("#ff0000", "#008080", "#003366"),
+    #                  name="", labels=c("Class I", "Class II and III", "Other TE")) +
+    theme(legend.position="top", axis.text=element_text(size=15), axis.title=element_text(size=15) ) +
+    scale_fill_discrete(name="", labels=c("Base-pairs", "COMET frequency"))
+#########
+s1
+
+###################################################################################################
+### SFS and genic features
+gen <- read.csv("cache/SFS_comet_features.csv")
+gen$exon <- gen$exon/sum(gen$exon)
+gen$intron <- gen$intron/sum(gen$intron)
+gen$onek <- gen$onek/sum(gen$onek)
+
+out2 <- gather(gen[, c("site", "onek", "exon", "intron")], "type", "value", 2:4)
+s2 <- ggplot(out2, aes(site, value, fill=type)) +
+    geom_bar(stat="identity", position = "dodge") +
+    #theme_bw() +
+    xlab("SFS") +
+    ylab("Frequency") +
+    #scale_fill_manual(values=c("#ff0000", "#008080", "#003366"),
+    #                  name="", labels=c("Exon", "Intron", "1k upstream")) +
+    scale_fill_discrete(name="", labels=c("Exon", "Intron", "1k upstream")) +
+    theme(legend.position="top", axis.text=element_text(size=15), axis.title=element_text(size=15) )
+#########
+s2
+
+###################################################################################################
+### SFS and transposons
+tab4 <- read.csv("cache/SFS_comet_features.csv")
+tab4$c1 <- tab4$c1/sum(tab4$c1)
+tab4$c2 <- tab4$c2/sum(tab4$c2)
+tab4$c3 <- tab4$c3/sum(tab4$c3)
+
+out3 <- gather(tab4[, c("site", "c1", "c2", "c3")], "type", "value", 2:4)
+s3 <- ggplot(out3, aes(site, value, fill=type)) +
+    geom_bar(stat="identity", position = "dodge") +
+    #theme_bw() +
+    xlab("SFS") +
+    ylab("Frequency") +
+    #scale_fill_manual(values=c("#ff0000", "#008080", "#003366"),
+    #                  name="", labels=c("Class I", "Class II and III", "Other TE")) +
+    scale_fill_discrete(name="", labels=c("Class I", "Class II and III", "Other TE")) +
+    theme(legend.position="top", axis.text=element_text(size=15), axis.title=element_text(size=15) ) 
+#########
+s3
+
+
+
+###################################################################################################
+### SFS and transposons
+### cowplot: combined
+p <- plot_grid(s1, s2, s3, ncol=1,rel_heights=c(1,1,1), align="v")
+
+pdf("graphs/Figure_sfs_cg.pdf", width=6, height=10)
+ggdraw(p) + draw_plot_label(c("A", "B", "C" ), c(0, 0, 0), c(1, 2/3, 1/3), size=15)
+dev.off()
+
+
+
+
+
+
+
