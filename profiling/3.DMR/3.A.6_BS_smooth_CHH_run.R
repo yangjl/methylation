aa6864c6eb9bcb525a4f96c5c1d785ecb8dc767f
Jinliang Yang <yangjl0930@gmail.com>
2016-12-08 10:07:58 -0800

    re-ran CHH smoothing

--- profiling/3.DMR/3.A.6_BS_smooth_CHH_run.R
+++ profiling/3.DMR/3.A.6_BS_smooth_CHH_run.R
@@ -11,4 +11,4 @@ run_Rcodes(inputdf=data.frame(file=1:20, out=1), outdir="slurm-script", cmdno=1,
            email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 16, "120G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
-###>>> RUN: sbatch -p bigmemm --mem 120G --ntasks=16 --time 24:00:00 slurm-script/run_rcode_array_chh.sh
+###>>> RUN: sbatch -p bigmemm --mem 120G --ntasks=16 --time 100:00:00 slurm-script/run_rcode_array_chh.sh
