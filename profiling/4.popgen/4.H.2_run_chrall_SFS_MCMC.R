8218d332a279e127688d06e146f3aa60f847350c
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-12-08 22:48:02 -0800

    updates

--- profiling/4.popgen/4.H.2_run_chrall_SFS_MCMC.R
+++ profiling/4.popgen/4.H.2_run_chrall_SFS_MCMC.R
@@ -5,11 +5,11 @@
 
 
 library("farmeR")
-run_Rcodes(inputdf=data.frame(file=1, out=1), outdir="slurm-script", cmdno=1,
+run_Rcodes(inputdf=data.frame(file=1:10, out=1:10), outdir="slurm-script", cmdno=1,
            rcodes = "profiling/4.popgen/4.H.1_chrall_SFS_MCMC.R",
-           arrayshid = "slurm-script/run_mcmc_array.sh",
-           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemh", 2, "16G"))
+           arrayshid = "slurm-script/run_mcmc_gc.sh",
+           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 1, "8G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
-###>>> RUN: sbatch -p bigmemm --mem 16G --ntasks=2 --time 48:00:00 slurm-script/run_rcode_array.sh
+###>>> RUN: sbatch -p bigmemm --mem 8G --ntasks=1 --time 24:00:00 slurm-script/run_mcmc_gc.sh
 
