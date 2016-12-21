18faa82dc8621b1d2b656b36b80b64c5a8dba255
Jinliang Yang <yangjl0930@gmail.com>
2016-12-20 17:06:44 -0800

    running chg_gazer

--- profiling/3.DMR/3.B.3_COMMETgazer_run.R
+++ profiling/3.DMR/3.B.3_COMMETgazer_run.R
@@ -27,8 +27,8 @@ for(i in 1:length(dirs)){
 library("farmeR")
 run_Rcodes(inputdf=data.frame(file=1:20, out=1), outdir="slurm-script", cmdno=1,
            rcodes = "profiling/3.DMR/3.B.2_COMMETgazer_CHG.R",
-           arrayshid = "slurm-script/run_rgazer_array.sh",
+           arrayshid = "slurm-script/run_chg_rgazer_array.sh",
            email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 2, "16G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
-####>>> sbatch -p bigmemm --mem 16G --ntasks=2 --time 100:00:00 slurm-script/run_rgazer_array.sh
+####>>> sbatch -p bigmemm --mem 16G --ntasks=2 --time 1000:00:00 slurm-script/run_chg_rgazer_array.sh
