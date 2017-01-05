279e816417d7cadbb4da05eb288b84a648530412
Jinliang Yang <yangjl0930@gmail.com>
2017-01-04 17:07:42 -0800

    hmmm, bad day!

--- profiling/3.COMET/3.A.6_BS_smooth_CHH_run.R
+++ profiling/3.COMET/3.A.6_BS_smooth_CHH_run.R
@@ -6,9 +6,9 @@
 
 library("farmeR")
 run_Rcodes(inputdf=data.frame(file=1:20, out=1), outdir="slurm-script", cmdno=1,
-           rcodes = "profiling/3.DMR/3.A.5_BS_smooth_CHH.R",
+           rcodes = "profiling/3.COMET/3.A.5_BS_smooth_CHH.R",
            arrayshid = "slurm-script/run_rcode_array_chh.sh",
-           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 16, "120G"))
+           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 16, "130G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
-###>>> RUN: sbatch -p bigmemm --mem 130G --ntasks=16 --nodelist=bigmem4,bigmem9 --time 1000:00:00 slurm-script/run_rcode_array_chh.sh
+###>>> RUN: sbatch -p bigmemm --mem 130G --ntasks=16 --time 1000:00:00 slurm-script/run_rcode_array_chh.sh
