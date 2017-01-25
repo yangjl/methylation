e7ac8e0e332da0e21c080bf82b64135fbdd86ea1
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-01-25 12:54:08 -0800

    re-order folder 5.MCMC_scan

--- profiling/rename.R
+++ profiling/rename.R
@@ -1,3 +1,6 @@
 
-files <- list.files(path="profiling/2.Teo20_WGBS", pattern="0.E.", full.names=T)
-file.rename(from=files, to=gsub("0.E.", "2.A", files))
+files <- list.files(path="profiling/5.MCMC_scan/", pattern="4.H.", full.names=T)
+file.rename(from=files, to=gsub("4.H.", "5.A", files))
+
+files <- list.files(path="profiling/5.MCMC_scan/", pattern="5.A", full.names=T)
+file.rename(from=files, to=gsub("5.A", "5.A.", files))
