e8ad52dc85a8f8a4950c1aeaf994062887f68da3
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-02-13 17:10:22 -0800

    setup run for gene feature

---
+++ profiling/3.COMET/3.E.1_feature_sum_run.R
@@ -0,0 +1,21 @@
+### Jinliang Yang
+### 12-01-2016
+### purpose: run BSmoothing
+
+cx <- c("largedata/COMET", "largedata/COMET_CHG", "largedata/COMET_CHH")
+
+df <- data.frame(pwd=rep(cx, each=1), output=c(1,2,3), context=rep(c("CG", "CHG", "CHH"), each=1))
+df$output <- paste0("cache/", df$context, "_chr1_gene_fea.csv")
+# col, pwd="largedata/COMET"
+# col: type="class=I"
+# col: output="cache/CG_chr1_TE_class1.csv"
+write.csv(df[3,], "largedata/run_gene_df.csv")
+
+library("farmeR")
+run_Rcodes(inputdf=data.frame(file=1:1, out=1), outdir="slurm-script", cmdno=1,
+           rcodes = "profiling/3.COMET/3.E.1_feature_summary.R",
+           arrayshid = "slurm-script/run_fea_array.sh",
+           email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 10, "80G"))
+
+###>>> In this path: cd /home/jolyang/Documents/Github/methylation
+###>>> RUN: sbatch -p bigmemm --time 24:00:00 --mem 80G --ntasks=10 slurm-script/run_fea_array.sh
--- profiling/3.COMET/3.E.1_feature_summary.R
+++ profiling/3.COMET/3.E.1_feature_summary.R
@@ -2,50 +2,47 @@
 ### 02-03-2017
 ### purpose: using smoothed data to summarize methylation levels
 
+##get command line args
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+
+JOBID <- as.numeric(as.character(args[1]))
+print(JOBID)
+
+
 library("data.table")
 library("GenomicRanges")
 library("tidyr")
 source("lib/find_cval_gff.R")
 
-######## format GFF and repeat files
-gff <- fread("~/dbcenter/AGP/AGPv2/ZmB73_5b_FGS.gff", header=TRUE, data.table=FALSE)
-names(gff) <- c("seqname", "source", "feature", "start", "end", "score",
-                "strand", "frame", "attribute")
-
-
-####### smoothed files CG
-pwd1 <- list.files(path="largedata/COMET", pattern="^J", full.names = TRUE)
-
-res1 <- data.frame()
-for(i in 1:length(pwd1)){
-    out1 <- find_cval_gff(infile=paste0(pwd1[i], "/chr1.txt"), gff)
-    res1 <- rbind(res1, out1)
+runit_gene <- function(df, id){
+    mypwd <- as.character(df$pwd[id])
+    #mytype <- as.character(df$type[id])
+    myout <- as.character(df$output[id]) 
+    
+    ######## format GFF and repeat files
+    gff <- fread("~/dbcenter/AGP/AGPv2/ZmB73_5b_FGS.gff", header=TRUE, data.table=FALSE)
+    names(gff) <- c("seqname", "source", "feature", "start", "end", "score",
+                    "strand", "frame", "attribute")
+    
+    ####### smoothed files CG
+    pwd1 <- list.files(path=mypwd, pattern="^J", full.names = TRUE)
+    
+    res1 <- data.frame()
+    for(i in 1:length(pwd1)){
+        out1 <- find_cval_gff(infile=paste0(pwd1[i], "/chr1.txt"), gff,
+                              features=c("exon", "intron", "up1k", "down1k", "gene", "CDS"))
+        res1 <- rbind(res1, out1)
+    }
+    write.csv(res1, myout)
 }
-write.csv(res1, "cache/CG_chr1_gff_feas.csv")
 
 
-####### smoothed files CHG
-pwd2 <- list.files(path="largedata/COMET_CHG/", pattern="^J", full.names = TRUE)
-
-res2 <- data.frame()
-for(i in 1:length(pwd2)){
-    out2 <- find_cval_gff(infile=paste0(pwd2[i], "/chr1.txt"), gff)
-    res2 <- rbind(res2, out2)
-}
-write.csv(res2, "cache/CHG_chr1_gff_feas.csv")
-
-
-####### smoothed files CHG
-pwd3 <- list.files(path="largedata/COMET_CHH/", pattern="^J", full.names = TRUE)
-
-res3 <- data.frame()
-for(i in 1:length(pwd3)){
-    out3 <- find_cval_gff(infile=paste0(pwd3[i], "/chr1.txt"), gff)
-    res3 <- rbind(res3, out3)
-}
-write.csv(res3, "cache/CHH_chr1_gff_feas.csv")
-
-
+## control elements
+df <- read.csv("largedata/run_gene_df.csv")
+# col, pwd="largedata/COMET"
+# col: output="cache/CG_chr1_TE_class1.csv"
+runit_gene(df, id=JOBID)
 
 
 
