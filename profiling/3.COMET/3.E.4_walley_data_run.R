569a168ef7e9246911e4f473b1852f65a609a3ee
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-02-14 13:13:01 -0800

    split Walley data into low and high

--- profiling/3.COMET/3.E.4_walley_data_run.R
+++ profiling/3.COMET/3.E.4_walley_data_run.R
@@ -3,26 +3,30 @@
 ### purpose: run BSmoothing
 
 
-cl <- c("open", "closed")
+cl <- c("low", "high")
+### additional file path
+af <- c("cache/geneset_rnaseq.csv", "cache/geneset_protein.csv")
 pwd <- list.files(path=c("largedata/COMET", "largedata/COMET_CHG", "largedata/COMET_CHH"), 
                   pattern="^J", full.names = TRUE)
 chr <- "chr1.txt"
 infile <- paste(pwd, chr, sep="/")
 
 
-df <- data.frame(infile=rep(infile, each=2), type=cl, output=c(1,2,3), 
-                 context=rep(c("CHG", "CHH", "CG"), each=40))
-df$output <- paste0("largedata/lcache/", df$context, "_chr1_chromtin_", df$type, "_",
+df <- data.frame(infile=rep(infile, each=4), af=rep(af, each=2), type=cl, 
+                 output=rep(c("RNA", "protein"), each=2), 
+                 context=rep(c("CHG", "CHH", "CG"), each=80))
+df$output <- paste0("largedata/lcache/", df$context, "_chr1_exp_", 
+                    df$output, "_", df$type, "_",
                     gsub(".*JR|\\/chr.*", "", df$infile), ".csv")
 # col, pwd="largedata/COMET"
 # col: type="class=I"
 # col: output="cache/CG_chr1_TE_class1.csv"
-write.csv(df, "largedata/run_chromatin_df.csv")
+write.csv(df, "largedata/run_exp_df.csv")
 
 library("farmeR")
-run_Rcodes(inputdf=data.frame(file=1:120, out=1), outdir="slurm-script", cmdno=1,
-           rcodes = "profiling/3.COMET/3.E.3_feature_chromatin.R",
-           arrayshid = "slurm-script/run_chromatin_array.sh",
+run_Rcodes(inputdf=data.frame(file=1:240, out=1), outdir="slurm-script", cmdno=1,
+           rcodes = "profiling/3.COMET/3.E.4_walley_data.R",
+           arrayshid = "slurm-script/run_exp_array.sh",
            email="yangjl0930@gmail.com", runinfo = c(FALSE, "bigmemm", 1, "8G"))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
--- profiling/5.MCMC_scan/5.C.1_data_mining_walley.R
+++ profiling/5.MCMC_scan/5.C.1_data_mining_walley.R
@@ -3,10 +3,10 @@
 ### Mining Walley et al., data
 
 # http://science.sciencemag.org/content/353/6301/814/tab-figures-data
-info <- read.csv("largedata/Walley_etal/info_maize_2016.csv")
+# info <- read.csv("largedata/Walley_etal/info_maize_2016.csv")
 
 fpkm <- read.csv("largedata/Walley_etal/aag1125_S1_mRNA_FPKM.csv")
-
+dim(fpkm)
 fgs <- subset(fpkm, Gene_Set %in% "filtered_set")
 set <- fgs[, 1:2]
 set$m <- apply(fgs[, 6:28], 1, mean)
